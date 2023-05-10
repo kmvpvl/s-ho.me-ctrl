eb6f1aa880be4f5e860133e15c7d08f031a739ae
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2023-05-10 22:59:39 +0300

    infinite cycle

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import dht from 'node-dht-sensor';
-const data = dht.read(22, 4);
-console.log(`temp=${data.temperature}; hum=${data.humidity}`);
+setInterval(()=> {
+    const data = dht.read(22, 4);
+    console.log(`temp=${data.temperature}; hum=${data.humidity}`);
+}, 30000);
