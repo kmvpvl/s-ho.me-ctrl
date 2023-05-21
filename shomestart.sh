b872d69607334e48fe1e43445fc3429bc9a312b5
shome <shome@s-ho.me>
2023-05-21 23:04:13 +0300

    logs to logs folder

--- shomestart.sh
+++ shomestart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 d=$(date '+%Y-%m-%d_%H_%M_%ST%Z')
-echo 'S-HO.ME service starting' >> $d.log
-node lib/index.js | tee $d.log
+echo 'S-HO.ME service starting' >> logs/$d.log
+node lib/index.js | tee logs/$d.log
